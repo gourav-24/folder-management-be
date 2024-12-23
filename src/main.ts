86e24439fe06a77a2a49e3af6a5e8c4c238cb0f6
Gourav Deswal <gouravdeswal@Gouravs-MacBook-Air.local>
2024-12-23 14:11:38 +0530

    configuring according to vercel

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,43 @@ import { ValidationPipe } from '@nestjs/common';
 
 
 
+import { Callback } from 'vercel';
+import serverlessExpress from '@vendia/serverless-express';
+
+let server: Callback;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // If you need to allow cookies
-  });
+  app.enableCors(); // Enable CORS if required
+  await app.init();
 
-  await app.listen(process.env.PORT ?? 8000);
+  const expressApp = app.getHttpAdapter().getInstance();
+  server = serverlessExpress({ app: expressApp });
 }
+
 bootstrap();
+
+export const handler: Callback = (event, context) => {
+  if (!server) {
+    bootstrap().then(() => server(event, context));
+  } else {
+    return server(event, context);
+  }
+};
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.useGlobalPipes(new ValidationPipe());
+//   app.enableCors({
+//     origin: 'http://localhost:3000', // Allow requests from this origin
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true, // If you need to allow cookies
+//   });
+
+//   await app.listen(process.env.PORT ?? 8000);
+// }
+// bootstrap();
 
 // TO:DO1: Run app
 // TO:DO2: Global try catch
