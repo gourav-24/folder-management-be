0df4c4365b4d650065d413b4d462286dcbaec4ed
Gourav Deswal <gouravdeswal@Gouravs-MacBook-Air.local>
2024-12-23 22:15:21 +0530

    allowed cors for FE

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
+    origin: 'https://folder-management-fe.vercel.app', // Allow requests from this origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // If you need to allow cookies
   });
